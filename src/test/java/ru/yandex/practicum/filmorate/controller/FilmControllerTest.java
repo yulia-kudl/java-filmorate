e3923a293ef36a13c91e5eb67ed9a9216bd5ecdd
Yulia Kudlai <iu.5hi@yandex.ru>
2025-06-02 20:29:01 +0300

    hw11

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -1,12 +1,5 @@
 package ru.yandex.practicum.filmorate.controller;
-import org.junit.jupiter.api.Test;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
-import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.service.FilmService;
 
-import java.time.LocalDate;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 //здесь нет тестов на валидацию полей, тк они проверяются через аннотации
 
--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -1,12 +1,5 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.junit.jupiter.api.Test;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
-import ru.yandex.practicum.filmorate.model.User;
-
-import java.time.LocalDate;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 //здесь нет тестов на валидацию полей, тк они проверяются через аннотации
 
